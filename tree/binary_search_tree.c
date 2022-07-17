7b13fe47614e8a4b4d5714cbb37948a0947ee5a2
Firman Gautama <1344876+sphinxid@users.noreply.github.com>
2022-07-17 20:40:15 +0700

    Create binary_search_tree.c

---
+++ tree/binary_search_tree.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int key;
+	struct node *left, *right;
+};
+
+struct node* newNode(int item)
+{
+	struct node* temp
+		= (struct node*)malloc(sizeof(struct node));
+	temp->key = item;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+void preorder(struct node* root)
+{
+    if(root != NULL) {
+        printf("%d ", root->key);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void inorder(struct node* root)
+{
+	if (root != NULL) {
+		inorder(root->left);
+		printf("%d ", root->key);
+		inorder(root->right);
+	}
+}
+
+void postorder(struct node* root)
+{
+    if(root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->key);
+    }
+}
+
+struct node* insert(struct node* node, int key)
+{
+	if (node == NULL)
+		return newNode(key);
+
+	if (key < node->key)
+		node->left = insert(node->left, key);
+	else if (key > node->key)
+		node->right = insert(node->right, key);
+
+	return node;
+}
+
+int main()
+{
+	struct node* root = NULL;
+    int i, len;
+    int data[] = {13, 27, 83, 67, 1, 90, 37, 40, 62, 102, 53, 72, 34, 21, 20};
+
+    len = sizeof(data)/sizeof(data[0]);
+    for(i=0;i<len;i++) {
+        if(i==0) {
+            root = insert(root, data[i]);
+        }
+        else {
+            insert(root, data[i]);
+        }
+    }
+
+    printf("Preorder: ");
+    preorder(root);
+    printf("\n");
+
+    printf("Inorder: ");
+	  inorder(root);
+    printf("\n");
+
+    printf("Postorder: ");
+	  postorder(root);
+    printf("\n");
+
+	return 0;
+}
